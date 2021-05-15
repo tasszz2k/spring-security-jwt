1219835f20e414477a9cf19da1345993a36051a4
tasszz2k <tasszz2k@gmail.com>
2021-05-15 10:26:42 +0700

    update

--- src/main/java/com/example/springsecurityjwt/HelloResource.java
+++ src/main/java/com/example/springsecurityjwt/HelloResource.java
@@ -2,12 +2,20 @@ package com.example.springsecurityjwt;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloResource {
 
-    @RequestMapping({"/hello"})
+    @RequestMapping("/")
+    public String hi() {
+        return "Hi";
+    }
+
+    @RequestMapping("/hello")
     public String hello() {
         return "Hello";
     }
+
+
 }
